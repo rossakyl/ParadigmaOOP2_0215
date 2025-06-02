47fa7a850940415cfe19a4fd31328bfd84016ba8
rossakyl <rossakayla01@gmail.com>
2025-06-02 18:25:08 +0700

    Implement display() method to print x and y

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -14,4 +14,11 @@ public:
         x = a;
         y = b;
     }
+    // menampilkan nilai
+    void display()
+    {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
 };
+
