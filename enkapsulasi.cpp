45716352ce315895f087e0abd153491c2866562e
rossakyl <rossakayla01@gmail.com>
2025-06-02 18:59:00 +0700

    tampilkan nama saklar dengan cout

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -28,8 +28,12 @@ int main()
     lampuRumah.setSaklarNo(2, "Lampu Teras Tidur");
     lampuRumah.setSaklarNo(3, "Lampu Teras Dapur");
 
+    cout << lampuRumah.getSaklarNo(0) <<endl;
+    cout << lampuRumah.getSaklarNo(1) <<endl;
+    cout << lampuRumah.getSaklarNo(2) <<endl;
+    cout << lampuRumah.getSaklarNo(3) <<endl;
     
     return 0;
 
-
+    
 }
