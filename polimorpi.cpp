74bdf2cf55bc03ec38738e727ea89d87102dc6ec
rossakyl <rossakayla01@gmail.com>
2025-06-02 17:38:07 +0700

    tambahkan class seseorang dengan fungsi virtual pesan()

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class seseorang 
+{
+public:
+    //virtual void pesan() = 0;
+    virtual void pesan()
+    {
+        cout << "Pesan dari seseorang" << endl;
+    }
+};
+
